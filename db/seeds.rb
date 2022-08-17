05f404040e23122a5bf18f734a0dee55820c477d
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-08-17 13:03:58 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(digits: 10), category: "chinese" }
+pizza_east = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(digits: 10), category: "italian" }
+popolare = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(digits: 10), category: "chinese" }
+mcdo = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(digits: 10), category: "chinese" }
+vietviet = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(digits: 10), category: "chinese" }
+
+[dishoom, pizza_east, popolare, mcdo, vietviet].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
